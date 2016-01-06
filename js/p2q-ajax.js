132342a414f56343d5284801e0faaf66f1edb584
Iain MacDonald <IJMacD@gmail.com>
2016-01-06 15:15:31 +0800

    Rebrand ajax file

--- js/p2q-ajax.js
+++ js/p2q-ajax.js
@@ -1,22 +1,22 @@
-	
-var frm = jQuery('#w2q-form');
+
+var frm = jQuery('#p2q-form');
 
 frm.submit(function (ev) {
 	ev.preventDefault();
-	
+
 	if (!confirm("Are you sure you've made a backup of your data?"))
 		return;
-	
-	jQuery('#w2q .spinner').show();
-	jQuery('#w2q-submit').attr('disabled', 'disabled');
-	
-	jQuery("#w2q-warnings ul").empty();
-	jQuery("#w2q-progress").empty();
-	
+
+	jQuery('#p2q .spinner').show();
+	jQuery('#p2q-submit').attr('disabled', 'disabled');
+
+	jQuery("#p2q-warnings ul").empty();
+	jQuery("#p2q-progress").empty();
+
 	var data = {};
 	jQuery(this).serializeArray().map(function(item) {
 		data[item.name] = item.value;
-	});	
+	});
 	submit(data);
 });
 
@@ -30,74 +30,74 @@ function submit(data) {
 
 			try {
 				jsonResult = jQuery.parseJSON(response);
-				
+
 				if (jsonResult.fatal) {
 					console.log("Fatal error: " + jsonResult.fatal);
-					jQuery("#w2q-warnings ul").append( jQuery('<li class="w2q-error" />' ).text( "Fatal error: " + jsonResult.fatal) );
+					jQuery("#p2q-warnings ul").append( jQuery('<li class="p2q-error" />' ).text( "Fatal error: " + jsonResult.fatal) );
 				} else {
 					jQuery.each(jsonResult.warnings, function(i, val) {
-						jQuery("#w2q-warnings ul").append( jQuery('<li class="w2q-warning" />' ).text( "Warning: " + val ) );
+						jQuery("#p2q-warnings ul").append( jQuery('<li class="p2q-warning" />' ).text( "Warning: " + val ) );
 					});
-					
-					jQuery("#w2q-progress").text( jsonResult.message );
+
+					jQuery("#p2q-progress").text( jsonResult.message );
 				}
-				
+
 				if (jsonResult.continue) {
 					//Remember all the variables
 					for (var attrname in jsonResult) { data[attrname] = jsonResult[attrname]; }
-					
+
 					submit(data);
 					return;
 				}
 			}
 			catch (e) {
 				console.log(e);
-				jQuery('#w2q-warnings ul').append( jQuery('<li class="w2q-error" />' ).text( "Fatal error: " + response ) );
+				jQuery('#p2q-warnings ul').append( jQuery('<li class="p2q-error" />' ).text( "Fatal error: " + response ) );
 			};
-			
-			jQuery('#w2q .spinner').hide();			
-			jQuery('#w2q-submit').removeAttr('disabled');
+
+			jQuery('#p2q .spinner').hide();
+			jQuery('#p2q-submit').removeAttr('disabled');
 		}
 	});
 }
 /*
 function ajaxSubmit ($) {
-	
+
 		var data =  jQuery(this).serialize();
-		//data = {  action: 'w2q_perform', w2q_nonce : w2q_vars.w2q_nonce };
-		
+		//data = {  action: 'p2q_perform', p2q_nonce : p2q_vars.p2q_nonce };
+
 		var perform = function() {
-		
+
 		$.post(ajaxurl, data, function(response) {
-		
+
 			try {
 				jsonResult = JSON.parse(response);
-				
+
 				if (jsonResult.fatal)
-					$('#w2q-progress').html(response.fatal);
+					$('#p2q-progress').html(response.fatal);
 				else {
 					jQuery.each(jsonResult.warnings, function(i, val) {
-					  $("#w2q-warnings").append( $('<li/>', {text: val }));
+					  $("#p2q-warnings").append( $('<li/>', {text: val }));
 					});
-					
-					$('#w2q-progress').html(jsonResult.message);
+
+					$('#p2q-progress').html(jsonResult.message);
 				}
-				
+
 				if (jsonResult.continue)
 					perform();
 			}
 			catch (e) {
-				console.log("error: "+e);				
-				$('#w2q-test').html("Failure");
+				console.log("error: "+e);
+				$('#p2q-test').html("Failure");
 			};
-			
-			$('#w2q .spinner').hide();				
-	  
+
+			$('#p2q .spinner').hide();
+
 		} );
-		
+
 		};
-		
-		$('#w2q .spinner').show();
+
+		$('#p2q .spinner').show();
 		perform();
 		return false;
 
